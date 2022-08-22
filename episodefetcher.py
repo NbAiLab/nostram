d3048aabac586abc2ae86ac55ff852cf93d5a0ad
Freddy Wetjen <freddy.wetjen@gmail.com>
2022-08-22 21:09:36 +0200

    fifth

--- episodefetcher.py
+++ episodefetcher.py
@@ -55,13 +55,27 @@ class episodefetcher:
         resp = self.geturl(programinforeq)
         resultingjson = resp.json()
         #print(resultingjson["seasons"][0]["name"])
-        return int(resultingjson["seasons"][0]["name"]) #vi bør kanskje sortere her
+        resultlist=[]
+        for ind,i in enumerate(resultingjson["seasons"]):
+            resultlist.append(resultingjson["seasons"][ind]["name"])
+        return resultlist
+
 
 
     def getprograms(self,title,seasonsnr):
-        programinforeq = "https://psapi.nrk.no/tv/catalog/series/kraakeklubben/seasons/1"
+        print(title)
+        programinforeq = "https://psapi.nrk.no/tv/catalog/series/"+ str(title) + "/seasons/" + str(seasonsnr)
         resp = self.geturl(programinforeq)
-        resultingjson = resp.json()["_embedded"]["episodes"]
+        print(programinforeq)
+        #exit(-1)
+        #print(resp)
+        #print(resp.json())
+        resultingjson =[]
+        if "episodes" in resp.json()["_embedded"]:
+            resultingjson = resp.json()["_embedded"]["episodes"]
+        else:
+            resultingjson = resp.json()["_embedded"]["instalments"]
+
         resultlist=[]
         for ind,i in enumerate(resultingjson):
             id=resultingjson[ind]["_links"]["self"]["href"].split("/")[-1]
@@ -80,7 +94,7 @@ if __name__ == '__main__':
     for i in inputlist.split(","):
         currentserie=ef.getseries(i)
         noseasons=ef.getmetadataforseries(currentserie)
-        for i in range(1,noseasons+1):
+        for i in noseasons:
             result=ef.getprograms(currentserie, i)
             for r in result:
                 print("serie" +currentserie + " sesong: " + str(i) + " id: " + str(r))
@@ -88,3 +102,37 @@ if __name__ == '__main__':
     #ef.getseries("DNPR63700111")
     #ef.getmetadataforseries("kraakeklubben")
     #ef.getprograms("kraakeklubben",1)
+
+    # MSUB22000113
+    # FBUA06000075
+    # MSUS01004710
+    # FBUA03003087
+    # MSUB20002611
+    # FBUB04000100
+    # FBUA03001389
+    # OBUB12000108
+    # FSTL01000188
+    # MKTV13100320
+    # MSUE13000118
+    # OBUB07000104
+    # OBUS01000103
+    # OBUB07000408
+    # FBUA03002588
+    # MSUS24000120
+    # MSUB02000110
+    # FBUA01007383
+    # MSUS05001110
+    # FALB60000192
+    # FBUA03000179
+    # DMND10005013
+    # FBUA03001388
+    # MSUB19120116
+    # FALU07000191
+    # MSUI40005120
+    # DMYT24002818
+    # DNPR63700110
+    # DNPR63000116
+
+
+
+
