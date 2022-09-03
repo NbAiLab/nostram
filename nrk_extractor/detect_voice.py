ee63e9d7a76da82b86cd6fc53d1e50057639fd08
Per E Kummervold <per@capia.no>
2022-09-03 10:20:55 +0200

    Try to avoid EOF error

--- nrk_extractor/detect_voice.py
+++ nrk_extractor/detect_voice.py
@@ -64,15 +64,19 @@ class VoiceDetector:
 
         Takes the path, and returns (PCM audio data, sample rate).
         """
-        with contextlib.closing(wave.open(path, 'rb')) as wf:
-            num_channels = wf.getnchannels()
-            assert num_channels == 1
-            sample_width = wf.getsampwidth()
-            assert sample_width == 2
-            sample_rate = wf.getframerate()
-            assert sample_rate in (8000, 16000, 32000, 48000)
-            pcm_data = wf.readframes(wf.getnframes())
-            return pcm_data, sample_rate
+        try:
+            with contextlib.closing(wave.open(path, 'rb')) as wf:
+                num_channels = wf.getnchannels()
+                assert num_channels == 1
+                sample_width = wf.getsampwidth()
+                assert sample_width == 2
+                sample_rate = wf.getframerate()
+                assert sample_rate in (8000, 16000, 32000, 48000)
+                pcm_data = wf.readframes(wf.getnframes())
+                return pcm_data, sample_rate
+        except:
+            print("**ERROR: Was not able to read the .wav file. Returning False")
+            return False
 
     def frame_generator(self, frame_duration_ms, audio, sample_rate):
         """Generates audio frames from PCM audio data.
