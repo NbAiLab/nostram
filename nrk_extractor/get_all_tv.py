384ef42aff6ba3779ac47ece0457685c19054a32
Per E Kummervold <per@capia.no>
2022-09-14 10:42:51 +0200

    get all radio and tv

---
+++ nrk_extractor/get_all_tv.py
@@ -0,0 +1,179 @@
+import requests
+import json
+import re
+import os
+import subprocess
+import time
+import sys
+import argparse
+import isodate
+import csv
+import dateutil.parser
+import jsonlines
+
+##################################################################################
+# Get all tv episodes and series - from category interface - creates a json file #
+##################################################################################
+
+def main(args):
+    error = 0
+    valid_manifest = 0
+    invalid_manifest = 0
+    tv_file = os.path.join(args.output_path,"tv.json")
+
+    with jsonlines.open(tv_file, mode='w') as writer:
+        seconds = 0
+        base_url = "https://psapi.nrk.no" 
+       
+        r = requests.get(base_url+"/tv/pages/")
+        if r.status_code != 200:
+            raise Exception("Failed to load metadata from '%s'" % murl)
+            
+        categories_json = json.loads(r.text)
+            
+        for category in categories_json['pageListItems']:
+            print(f"\nProcessing Category {category['id']}")
+            
+            all_url = base_url+category['_links']['self']['href']
+            all_json = get_json(all_url)
+            
+            for section in all_json['sections']:
+                print(f"\n-Proceccing Section {section['included']['title']}")
+                
+
+                for n,item in enumerate(section['included']['plugs']):
+                    itemtype = item['targetType']
+
+                    if 'series' == itemtype:
+                        print(f"\n--#{n} Processing {itemtype} {item['displayContractContent']['contentTitle'].strip()}")
+                        
+                        serie_json = get_json(base_url+item[item['targetType']]['_links']['self']['href'])
+                        if not serie_json:
+                            print("\n\n***ERROR Serie Json\n")
+                            continue
+                    
+                        if 'seasons' in serie_json:
+                            for season in serie_json['seasons']:
+                                print(f"\n--season - {season['name']}")
+                                season_json = get_json(base_url+'/tv/catalog/series/'+serie_json['id']+'/seasons/'+season['name'])
+                                 
+                                if not season_json: 
+                                    print("\n\n***ERROR Season Json\n")
+                                    continue
+                                
+                                for episode in season_json['_embedded']['episodes']:
+                                    episode_seconds = write_episode(episode,writer,season_json['image'][0]['url'])
+                                    seconds += episode_seconds
+                                    if episode_seconds:
+                                        valid_manifest += 1
+                                    else:
+                                        invalid_manifest += 1
+                        else:
+                            print("This happens for radio, but I have not found it in TV. It is therefore untested. If it crashes here, uncomment, and restart")
+                            exit(-1)
+                            for episode in serie_json['_embedded']['episodes']:
+                                episode_seconds = write_episode(episode, writer, serie_json['series']['image'][0]['url'])
+                                seconds += episode_seconds
+                                
+                                if episode_seconds:
+                                    valid_manifest += 1
+                                else:
+                                    invalid_manifest += 1
+                    else:
+                        print("This should not happen!")
+                        breakpoint()
+                        print(item['_links'])
+                        exit(-1)
+
+        print(f"\nTotal time: {round(seconds/3600)} hours.") 
+        print(f"\nThere were a total of {valid_manifest} episodes with valid manifest files, and {invalid_manifest} episodes with an invalid one.")
+        print(f"\nFinished writing json output file to {(podcast_file)}")
+
+def write_episode(episode,writer,serie_image_url="None"):
+        base_url = "https://psapi.nrk.no"   
+        episode_id = episode['prfId']
+        medium = episode['_links']['self']['href'].split("/")[1] 
+        program_image_url = episode['image'][0]['url']
+
+        title = episode['titles']['title']
+        subtitle = episode['titles']['subtitle']
+        year = episode['productionYear']
+        
+        #Availability - Take this from the manifest-file since it is more accurate
+        # Get the playback file
+        playback_json = get_json(base_url+episode['_links']['playbackmetadata']['href'])
+
+        #Get the manifest-file
+        try:
+            manifest_json = get_json(base_url+playback_json['_links']['manifests'][0]['href'])
+
+            availability_information = manifest_json['availability']['information']
+            is_geoblocked = manifest_json['availability']['isGeoBlocked']
+        
+            duration = round(isodate.parse_duration(manifest_json['playable']['duration']).total_seconds())
+            audio_file = manifest_json['playable']['assets'][0]['url']
+            audio_format = manifest_json['playable']['assets'][0]['format']
+            audio_mime_type = manifest_json['playable']['assets'][0]['mimeType']
+            manifest_exist = True
+        except:
+            #No manifest-file exists
+            is_geoblocked = "unknown"
+            availability_information = ""
+            duration = 0
+            audio_file = "unknown"
+            audio_format = "unknown"
+            audio_mime_type = "unknown"
+            manifest_exist = False
+            print('M', end='', flush=True)
+
+        try:
+            on_demand_from = manifest_json['availability']['onDemand']['from']
+            on_demand_to  = manifest_json['availability']['onDemand']['to']        
+        except:
+            on_demand_from = "undefined"
+            on_demand_to = "undefined"
+
+        row = {'episode_id':episode_id, 
+                'medium': medium, 
+                'programi_image_url': program_image_url, 
+                'serie_image_url':serie_image_url,
+                'title':title,
+                'subtitle': subtitle,
+                'year':year,
+                'duration': duration,
+                'availability_information':availability_information,
+                'is_geoblocked':is_geoblocked,
+                'on_demand_from':on_demand_from,
+                'on_demand_to':on_demand_to,
+                'audio_file':audio_file,
+                'audio_format':audio_format,
+                'audio_mime_type':audio_mime_type}
+        
+        if manifest_exist:
+            writer.write(row)
+            print('.', end='', flush=True)
+        
+        return duration
+
+def get_json(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return False   
+
+    return json.loads(r.text)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output_path', help="Complete path to json output file. The exact name will be given in the program.", required=True)
+
+    args = parser.parse_args()
+    main(args)
+
+
+
+
+
+
+
+
---
+++ nrk_extractor/get_all_tv_old.py
@@ -0,0 +1,239 @@
+import requests
+import json
+import re
+import os
+import subprocess
+import time
+import sys
+import argparse
+import isodate
+import csv
+import dateutil.parser
+
+##################################################################################
+# Get all tv episodes and series - from category interface - creates a json file #
+##################################################################################
+
+
+def main(args):
+    error = 0
+    tv_file = os.path.join(args.output_path,"tv.json")
+    
+    with jsonlines.open(tv_file, mode='w') as writer:
+            seconds = 0
+            base_url = "https://psapi.nrk.no" 
+            r = requests.get(base_url+"/tv/pages/")
+            if r.status_code != 200:
+                raise Exception("Failed to load metadata from '%s'" % murl)
+            
+            categories_json = json.loads(r.text)
+
+            #page_items = "pageListItems"
+            #target_type ="targetType"
+
+            for category in categories_json[pageListItems]:
+                print(f'\n\n* Parsing {categories["_links"]["self"]["href"]}')
+                murl = "https://psapi.nrk.no/"+category["_links"]["self"]["href"]
+                
+                r = requests.get(murl)
+                if r.status_code != 200:
+                    raise Exception("Failed to load metadata from '%s'" % murl)
+                serie_json = json.loads(r.text)
+                
+                #Loop through series and standalones
+                ''' 
+                catcount = 0
+                for series in res["series-"+medium]["sections"]:
+                    
+                    #For each of these check how long they are in total
+                    for programs in series["included"]["plugs"]:
+                        seconds = 0
+                        num_episodes = 0
+                        first_episode_id = 0
+                        all_episodes_ids = []
+                        first_prod_year = 2022
+                        last_prod_year = 0
+
+                        #print(programs[target_type])
+                        if programs[target_type] == "series":
+                            try:
+                                theserie = get_serie_json(programs['series']['_links']['self']['href'], medium)
+                                program_title = programs['displayContractContent']['contentTitle']
+                            except:
+                                theserie = get_serie_json(programs['_links']['series'],medium)
+                                program_title = programs['series']['titles']['title']
+
+                            
+                            try:
+                                for seasons in theserie['_embedded']['seasons']:
+                                    #Loop through the episodes
+                                    #The errors here seem to be stuff that are not episodes. Just ignore
+                                    single = False
+
+                                    try:
+                                        for episodes in seasons['_embedded']['episodes']:
+                                            num_episodes += 1
+                                            all_episodes_ids.append(episodes['prfId'])
+                                            if episodes['productionYear'] <= first_prod_year:
+                                                first_prod_year = episodes['productionYear']
+                                            if episodes['productionYear'] >= last_prod_year:
+                                                last_prod_year = episodes['productionYear']
+                                            if first_episode_id == 0:
+                                                first_episode_id = episodes['prfId']
+                                        
+                                            seconds += isodate.parse_duration(episodes['duration']).total_seconds()
+                                    except:
+                                        #breakpoint()
+                                        #print("Missing data. Skipping. Just ignore this if it is not too many.")
+                                        error = 1
+                                        continue
+                            except:
+                                #Runs only if there are no seasons
+                                season = theserie['_embedded']['episodes']
+                                for episodes in season['_embedded']['episodes']:
+                                            num_episodes += 1
+                                            all_episodes_ids.append(episodes['_links']['self']['href'].split('/')[-1])
+                                            if episodes['productionYear'] <= first_prod_year:
+                                                first_prod_year = episodes['productionYear']
+                                            if episodes['productionYear'] >= last_prod_year:
+                                                last_prod_year = episodes['productionYear']
+                                            if first_episode_id == 0:
+                                                first_episode_id = episodes['_links']['self']['href'].split('/')[-1]
+
+                                            seconds += isodate.parse_duration(episodes['duration']).total_seconds()
+
+
+                        elif (programs[target_type] == "standaloneProgram") or (programs[target_type] == "episode"):
+                            
+                            #Get meta information for this program
+                            try:
+                                theprogram = get_program_json(programs[programs[target_type]]['_links']['self']['href'])
+                            except:
+                                try:
+                                    theprogram = get_program_json(programs['_links'][programs[target_type]])
+                                except:
+                                    theprogram = get_program_json(programs['_links']['program'])
+
+                            first_prod_year = last_prod_year = theprogram['productionYear']
+                            seconds += isodate.parse_duration(theprogram['duration']).total_seconds()
+                            first_episode_id = theprogram['id']
+                            all_episodes_ids.append(theprogram['id'])
+                            
+                            try:
+                                program_title = programs['displayContractContent']['contentTitle']
+                            except:
+                                try:
+                                    program_title = programs[programs[target_type]]['titles']['title']
+                                except:
+                                    program_title = programs['program']['titles']['title']
+
+                            num_episodes = 1
+                            single = True
+                       
+                        elif programs[target_type] == "podcastEpisode":
+                            theprogram = get_program_json(programs['_links']['podcastEpisode'])
+                            seconds += isodate.parse_duration(theprogram['duration']).total_seconds()
+                            first_prod_year = last_prod_year = dateutil.parser.isoparse(theprogram['publishedAt']).year
+                            first_episode_id = theprogram['_links']['self']['href'].split('/')[-1]
+                            all_episodes_ids.append(first_episode_id)
+                            program_title = programs['podcastEpisode']['titles']['title']
+                            num_episodes = 1
+                            single = True
+                        
+                        elif programs[target_type] == "podcast":
+                            thepodcast = get_podcast_json(programs['_links']['podcast'])
+
+                            for episode in thepodcast['_embedded']['episodes']['items']:
+                                year = dateutil.parser.isoparse(episode['publishedAt']).year
+                                if year <= first_prod_year:
+                                            first_prod_year = year
+                                if year >= last_prod_year:
+                                            last_prod_year = year
+                                seconds += isodate.parse_duration(episode['duration']).total_seconds()
+                                if first_episode_id == 0:
+                                    first_episode_id = episode['_links']['podcastEpisode']['href'].split('/')[-1]
+                                all_episodes_ids.append(first_episode_id)
+                                num_episodes = len(thepodcast['_embedded']['episodes']['items'])
+
+                            program_title = programs['podcast']['titles']['title']
+                            num_episodes = programs['podcast']['numberOfEpisodes']
+                            single = False
+
+                        elif programs[target_type] == "channel":
+                            print("Channel - skipping!")
+                            continue
+
+                        else:
+                            print("Unknown - skipping!")
+                            continue
+
+                        if error == 0:
+                            #Clean up variables
+                            if len(series['included']['title']) <= 1:
+                                cat_title = "Undefined"
+                            else:
+                                cat_title = series['included']['title']
+                            
+
+                            #print(f"{cat_title} -  {program_title} - {first_prod_year} - {last_prod_year} - {nr_episodes} - {first_episode_id} - array({len(all_episodes_ids)}) - {round(seconds)}")
+
+                            row = {'category': cat_title,'title': program_title,'first_prod_year':first_prod_year,'last_prod_year':last_prod_year,'num_episodes':num_episodes,'first_episode_id':first_episode_id,'all_episodes_ids':str(all_episodes_ids),'total_seconds':round(seconds)}
+                            #print(".", end = '') 
+                            print('.', end='', flush=True)
+                            #print(row)
+                            
+                            if single == True:
+                                fsi_writer.writerows([row])       
+                            else:
+                                fse_writer.writerows([row])
+
+                        else:
+                            error = 0
+                '''
+
+    print("Finished writing csv output file to "+str(args.output_file))
+
+def get_serie_json(seriename,medium):
+    surl = "https://psapi.nrk.no/"+medium+"/catalog"+seriename
+
+    r = requests.get(surl)
+    if r.status_code != 200:
+        raise Exception("Failed to load metadata from '%s'" % surl)
+
+    return(json.loads(r.text))
+
+def get_podcast_json(podcast):
+    surl = "https://psapi.nrk.no/"+podcast
+
+    r = requests.get(surl)
+    if r.status_code != 200:
+        raise Exception("Failed to load metadata from '%s'" % surl)
+
+    return(json.loads(r.text))
+
+
+def get_program_json(programpath):
+    surl = "https://psapi.nrk.no"+programpath
+
+    r = requests.get(surl)
+    if r.status_code != 200:
+        raise Exception("Failed to load metadata from '%s'" % surl)
+
+    return(json.loads(r.text))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--medium', help="Medium is either radio or tv", required=True)
+    parser.add_argument('--output_path', help="Complete path to csv output file. Note that it will split this in two names with _singles and _series by itself", required=True)
+
+
+    args = parser.parse_args()
+    main(args)
+
+
+
+
+
+
+
+
