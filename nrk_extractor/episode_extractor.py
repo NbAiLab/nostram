3f05524627d20cbc4db19411479e3ccd538232f5
Javier de la Rosa <versae@gmail.com>
2022-09-16 13:01:55 +0200

    Add audio segment extraction

--- nrk_extractor/episode_extractor.py
+++ nrk_extractor/episode_extractor.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
-
 """
 NORCE Research Institute 2022, Njaal Borch <njbo@norceresearch.no>
 Licensed under Apache 2.0
 
-Modified by Freddy Wetjen and Per Egil Kummervold
+Modified by Freddy Wetjen, Per Egil Kummervold, and Javier de la Rosa
 National Library of Norway
 """
-
-import requests
 import json
-import re
 import os
 import subprocess
-import time
-import sys
-
 from argparse import ArgumentParser
-from detect_voice import VoiceDetector
-from sub_parser import SubParser
-from fetch_episodes import episodefetcher
+
 import jsonlines
+from detect_voice import VoiceDetector
+from tqdm import tqdm
 
 
 class EpisodeExtractor():
 
-    def extract_audio(self, info, target):
+    def extract_audio(self, url, target, start=None, end=None):
         if os.path.exists(target) and os.stat(target).st_size > 0:
-            print("Audio already present at '%s'" % target)
+            if start is None or end is None:
+                print("Audio already present at '%s'" % target)
             return
-        
-        url = info["audio_file"]
-        cmd = ["ffmpeg", "-y", "-i", url, "-vn", "-c:a", "copy", target]
 
-        print("Extracting audio to %s" % target)
+        if start is None or end is None:
+            cmd = ["ffmpeg", "-y", "-i", url, "-vn", "-c:a", "copy", target]
+            print(f"Extracting audio to %s" % target)
+        else:
+            cmd = ["ffmpeg", "-y", "-i", url, "-ss", f"{start}ms", "-to", f"{end}ms", "-vn", "-c:a", "copy", target]
+
         p = subprocess.run(cmd,
                            stdout=subprocess.DEVNULL,
                            stderr=subprocess.STDOUT)
@@ -42,19 +38,41 @@ class EpisodeExtractor():
         if p.returncode != 0:
             raise Exception("Extraction failed '%s', code %s" % (cmd, p))
 
+    def extract_segment(self, entry, audio, audio_segments_dir):
+        audio_segment_filename = f"{entry['episode_id']}_{entry['start_time']}_{entry['end_time']}_{entry['duration_ms']}.wav"
+        audio_segment_dir = audio_segments_dir + f"/{entry['episode_id']}"
+
+        if not os.path.exists(audio_segment_dir):
+            os.makedirs(audio_segment_dir)
+
+        audio_segment_destinaton = os.path.join(
+            audio_segment_dir, audio_segment_filename
+        )
+        self.extract_audio(
+            audio,
+            audio_segment_destinaton,
+            start=entry['start_time'],
+            end=entry['end_time']
+        )
 
     def dump_at(self, info, target_dir):
         id = info['episode_id']
 
         if not os.path.exists(target_dir):
             os.makedirs(target_dir)
-        
-        if not os.path.exists(target_dir+"/audio"):
-            os.makedirs(target_dir+"/audio")
-        
-        if not os.path.exists(target_dir+"/segments"):
-            os.makedirs(target_dir+"/segments")
-        
+
+        audio_dir = target_dir + "/audio"
+        if not os.path.exists(audio_dir):
+            os.makedirs(audio_dir)
+
+        segments_dir = target_dir + "/segments"
+        if not os.path.exists(segments_dir):
+            os.makedirs(segments_dir)
+
+        audio_segments_dir = target_dir + "/audio_segments"
+        if not os.path.exists(audio_segments_dir):
+            os.makedirs(audio_segments_dir)
+
         if info['audio_format'] == "HLS":
             audio_file_name = f"{id}.mp4"
         elif info['audio_format'] == "MP3":
@@ -63,14 +81,13 @@ class EpisodeExtractor():
             print("Error: Unknown Audio Format")
             exit(-1)
 
-        target_audio = os.path.join(target_dir+"/audio", audio_file_name)
-        
-        self.extract_audio(info, target_audio)
-        
-        destination = os.path.join(target_dir, 'segments', id+'.json')
-       
+        target_audio = os.path.join(target_dir, "audio", audio_file_name)
+        target_segment = os.path.join(target_dir, 'segments', id + '.json')
+
+        self.extract_audio(info["audio_file"], target_audio)
+
         subtitles = self.resync(target_audio,  options)
-        self.save_jsonlines(subtitles, destination, info)
+        self.save_jsonlines(subtitles, target_segment, info, target_audio, audio_segments_dir)
 
         return info
 
@@ -79,40 +96,41 @@ class EpisodeExtractor():
         segments = detector.analyze(aggressive=options.aggressive,
                                     max_pause=float(options.max_pause),
                                     max_segment_length=float(options.max_segment_length))
-        
+
         return segments
-    
-    def save_jsonlines(self, segments, destination, info):
-        def build_entry(item, info):
-            entry = {
-                    "id": info["episode_id"]+"_"+str(int(item["start"]*100))+"_"+str(int(item["end"]*100)),
+
+    def save_jsonlines(self, segments, destination, info, audio, audio_segments_dir):
+        # Save segments
+        with open(destination, "w") as destination_file:
+            # Write a single line pr entry that's good
+            segments_iter = tqdm(
+                segments, desc="Saving audio segments", total=len(segments)
+            )
+            for item in segments_iter:
+                entry = {
+                    "id": info["episode_id"] + "_" + str(int(item["start"] * 100)) + "_" + str(int(item["end"] * 100)),
                     "start_time": int(item["start"]*100),
                     "end_time": int(item["end"]*100),
                     "duration_ms": int((item["end"] - item["start"])*100),
-                    'episode_id':info['episode_id'], 
-                    'medium': info['medium'], 
-                    'program_image_url': info['program_image_url'], 
-                    'serie_image_url':info['serie_image_url'],
-                    'title':info['title'],
-                    'sub_title': info['subtitle'], 
-                    'year':info['year'],
-                    'availability_information':info['availability_information'],
-                    'is_geoblocked':info['is_geoblocked'],
-                    'external_embedding_alowed':info['external_embedding_allowed'],
-                    'on_demand_from':info['on_demand_from'],
-                    'on_demand_to':info['on_demand_to'],
-                    'audio_file':info['audio_file'],
-                    'audio_format':info['audio_format'],
-                    'audio_mime_type':info['audio_mime_type']}
+                    'episode_id': info['episode_id'],
+                    'medium': info['medium'],
+                    'program_image_url': info['program_image_url'],
+                    'serie_image_url': info['serie_image_url'],
+                    'title': info['title'],
+                    'sub_title': info['subtitle'],
+                    'year': info['year'],
+                    'availability_information': info['availability_information'],
+                    'is_geoblocked': info['is_geoblocked'],
+                    'external_embedding_alowed': info['external_embedding_allowed'],
+                    'on_demand_from': info['on_demand_from'],
+                    'on_demand_to': info['on_demand_to'],
+                    'audio_file': info['audio_file'],
+                    'audio_format': info['audio_format'],
+                    'audio_mime_type': info['audio_mime_type']
+                }
+                destination_file.write(json.dumps(entry) + "\n")
+                self.extract_segment(entry, audio, audio_segments_dir)
 
-            return entry
-
-        # Save segments
-        with open(destination, "w") as f:
-            # Write a single line pr entry that's good
-            for idx, item in enumerate(segments):
-                entry = build_entry(item,info)
-                f.write(json.dumps(entry) + "\n")
 
 if __name__ == "__main__":
 
@@ -149,10 +167,10 @@ if __name__ == "__main__":
 
     extractor = EpisodeExtractor()
 
-    print("\n\n* Starting processing "+options.src)
+    print("\n\n* Starting processing " + options.src)
 
     with jsonlines.open(options.src) as reader:
         for obj in reader:
             info = extractor.dump_at(obj, options.dst)
-              
+
 
