d12eec1776ac5ea49e86d892d4cb9c1ea84ef71e
Per Egil Kummervold <per@capia.no>
2022-09-29 12:47:17 +0000

    first submit

---
+++ wav2vec_pretrain/run_backup.sh
@@ -0,0 +1,20 @@
+
+MODEL_DIR="./"
+python ./run_wav2vec2_pretrain_flax.py \
+    --output_dir=${MODEL_DIR} \
+    --num_train_epochs="5" \
+    --per_device_train_batch_size="32" \
+    --per_device_eval_batch_size="32" \
+    --learning_rate="5e-4" \
+    --weight_decay="0.01" \
+    --warmup_steps="2000" \
+    --model_name_or_path=${MODEL_DIR} \
+    --dataset_name="librispeech_asr" \
+    --dataset_config_name="clean" \
+    --train_split_name="train.100" \
+    --preprocessing_num_workers="4" \
+    --max_duration_in_seconds="10.0" \
+    --adam_beta1="0.9" \
+    --adam_beta2="0.98" \
+    --pad_to_multiple_of="16384" \
+    --push_to_hub
