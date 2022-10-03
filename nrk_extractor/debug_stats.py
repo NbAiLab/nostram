c898c1a97af81650987868c376ced4979c506687
Per E Kummervold <per@capia.no>
2022-10-03 08:38:14 +0200

    test

---
+++ nrk_extractor/debug_stats.py
@@ -0,0 +1,33 @@
+
+import json
+import sys
+import os
+import jsonlines
+from argparse import ArgumentParser
+import pandas as pd
+import glob
+import urllib.request
+
+###################################################
+# Debug script made for detecting missing subtitles
+###################################################
+
+def main(args):
+    target_file = os.path.join(args.directory,'process_list/','tv.json')
+    
+    data = pd.read_json(target_file, lines=True) # read data frame from json file
+
+    breakpoint()
+
+
+def parse_args():
+    # Parse commandline
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--directory", dest="directory", help="Directory to Json-lines file to analyse", required=True)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
