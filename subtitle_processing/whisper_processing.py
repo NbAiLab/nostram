846383d6cb3e28eb07cc5a60e8ccfe33064aba45
Per Egil Kummervold <per@capia.no>
2023-05-03 07:42:49 +0000

    whisper processing for pods

--- subtitle_processing/whisper_processing.py
+++ subtitle_processing/whisper_processing.py
@@ -1,21 +1,26 @@
+import jax
 import jax.numpy as jnp
 from datasets import load_dataset
+from datasets.distributed import split_dataset_by_node
 from whisper_jax import FlaxWhisperPipline
 from jiwer import wer, cer
 import jsonlines
 import numpy as np
+import os
 
-def process_dataset(dataset, output_folder):
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+os.environ['CMALLOC_VERBOSE'] = '0'
+os.environ['TCMALLOC_VERBOSE'] = '0'
+os.environ['TCMALLOC_LARGE_ALLOC_REPORT_THRESHOLD'] = '10000000000'
+
+def process_dataset(dataset, output_filename):
     pipeline = FlaxWhisperPipline("openai/whisper-large-v2", dtype=jnp.bfloat16, batch_size=16)
-
-    output_filename = "processed_output.jsonl"
-    output_path = f"{output_folder}/{output_filename}"
 
     total_wer = 0
     total_cer = 0
     count = 0
 
-    with jsonlines.open(output_path, "w") as output_file:
+    with jsonlines.open(output_filename, "w") as output_file:
         for example in dataset:
             audio = {
                 'array': np.array(example['audio']['array']),
@@ -48,9 +53,15 @@ def process_dataset(dataset, output_folder):
     print(f"Total CER: {total_cer / count}")
 
 def main():
-    dataset = load_dataset("NbAiLab/NCC_speech_v5_mini", split="test", streaming=True)
-    output_folder = '/home/perk/models/output'
-    process_dataset(dataset, output_folder)
+    num_of_hosts = jax.process_count()
+    current_host_idx = jax.process_index()
+
+    dataset = load_dataset("NbAiLab/NCC_speech_v5", split="train", streaming=True)
+    node_dataset = split_dataset_by_node(dataset, rank=current_host_idx, world_size=num_of_hosts)
+
+
+    output_filename = f'/home/perk/models/output/processed_idx{current_host_idx}.json'
+    process_dataset(node_dataset, output_filename)
 
 if __name__ == "__main__":
     main()
