270a27a4f97b1fbfaa8afab3cf7beeee0144d53a
Per E Kummervold <per@capia.no>
2023-09-05 11:45:22 +0200

    json stats

--- utils/json_stats.py
+++ utils/json_stats.py
@@ -2,41 +2,57 @@ import os
 import argparse
 
 def format_number(number):
+    """Format the number with commas and right-justify it."""
     return '{:,}'.format(number).rjust(10)
 
 def get_json_stats(target_dir):
+    """Walk through the target directory and collect stats about JSON files."""
     stats = {}
     for root, dirs, files in os.walk(target_dir):
         json_files = [f for f in files if f.endswith('.json')]
-        if json_files:
-            relative_root = os.path.relpath(root, target_dir)
-            stats[relative_root] = {}
-            for json_file in json_files:
-                file_path = os.path.join(root, json_file)
-                with open(file_path, 'r') as f:
-                    line_count = sum(1 for _ in f)
-                stats[relative_root][json_file] = line_count
+        relative_root = os.path.relpath(root, target_dir)
+        stats[relative_root] = {}
+        for json_file in json_files:
+            file_path = os.path.join(root, json_file)
+            with open(file_path, 'r') as f:
+                line_count = sum(1 for _ in f)
+            stats[relative_root][json_file] = line_count
     return stats
 
-def generate_markdown(stats):
-    print(f"## Target Directory: {args.target_dir}")
-    print("| Directory | File | Lines     |")
-    print("| --------- | ---- | ---------:|")
-    
-    for dir_path, json_stats in stats.items():
-        indents = dir_path.count(os.sep)
-        padding = "&nbsp;" * (indents * 8) 
+def generate_markdown(stats, target_dir):
+    """Generate Markdown tables for the collected stats."""
+    print(f"## Target Directory: {target_dir}")
+
+    first_level_dirs = {k.split(os.sep)[0] for k in stats.keys() if k != '.'}
+
+    for first_level_dir in sorted(first_level_dirs):
+        print(f"### Directory: {first_level_dir}")
+        print("| Directory | File | Lines     |")
+        print("| --------- | ---- | ---------:|")
         
-        for json_file, line_count in json_stats.items():
-            formatted_count = format_number(line_count)
-            level = f"{padding}{dir_path}"
-            print(f"| {level} | {json_file} | {formatted_count} |")
+        total_lines = 0
+
+        for dir_path, json_stats in sorted(stats.items()):
+            if not dir_path.startswith(first_level_dir):
+                continue
+
+            if not json_stats:
+                print(f"| {dir_path} | <empty> | <empty>    |")
+                continue
+            
+            for json_file, line_count in json_stats.items():
+                total_lines += line_count
+                formatted_count = format_number(line_count)
+                print(f"| {dir_path} | {json_file} | {formatted_count} |")
+
+        print(f"| **Total** |      | **{format_number(total_lines)}** |")
+        print()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate Markdown file with JSON stats.")
     parser.add_argument("target_dir", help="Target directory to start the search.")
     args = parser.parse_args()
-    
+
     stats = get_json_stats(args.target_dir)
-    generate_markdown(stats)
- 
+    generate_markdown(stats, args.target_dir)
+
