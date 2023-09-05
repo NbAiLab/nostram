a56a1aaac4e76acfce250645ddbf3cecf4b18131
Per E Kummervold <per@capia.no>
2023-09-05 15:11:15 +0200

    duplicates

--- utils/duplicate_stats.py
+++ utils/duplicate_stats.py
@@ -12,18 +12,18 @@ def main(input_file, min_duplicates):
     filtered_count = duplicate_count[duplicate_count >= min_duplicates]
     
     # Generate Markdown Table
-    print("| text | number_of_duplicates | id's |")
-    print("|------|----------------------|------|")
+    print("| text | number_of_duplicates |") # id's |")
+    print("|------|----------------------|") #------|")
     
     for text, num in filtered_count.items():
         # Find ids corresponding to the duplicate text
-        ids = ', '.join(map(str, df[df['text'] == text]['id'].tolist()))
+        #ids = ', '.join(map(str, df[df['text'] == text]['id'].tolist()))
         
         # HTML code for expandable id's
-        expandable_html = f"<details><summary>Show/Hide</summary><p>{ids}</p></details>"
+        #expandable_html = f"<details><summary>Show/Hide</summary><p>{ids}</p></details>"
         
         # Print entire sentence (text), number of duplicates and expandable ids in Markdown table format
-        print(f"| {text} | {num} | {expandable_html} |")
+        print(f"| {text} | {num} |") # {expandable_html} |")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
