5b1bddf08b4a7008cda0e874db8f4c5e7543fcf5
pere <per@capia.no>
2023-09-07 18:06:26 +0200

    Update clean_text.py

--- utils/clean_text.py
+++ utils/clean_text.py
@@ -48,7 +48,7 @@ def clean_text(text, verbose=False):
     original_text = text
 
     # Delete line if it contains any bracket and a few other weird characters
-    if "(" in text or ")" in text or "[" in text or "]" in text or "{" in text or "}" in text or "ã" in text:
+    if "(" in text or ")" in text or "[" in text or "]" in text or "{" in text or "}" in text or "ã" in text or "ú" in text:
         stats["delete_line"] = True
         if verbose: print(f"Line to be deleted - Original: {original_text}")
         return text, stats
