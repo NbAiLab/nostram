eb1c75f56ff9adf56d9575c62bb686cc2426f9d6
pere <per@capia.no>
2023-09-07 21:07:21 +0200

    Update clean_text.py

--- utils/clean_text.py
+++ utils/clean_text.py
@@ -105,7 +105,7 @@ def clean_text(text, verbose=False):
         return text, stats
     
     # Delete line if it contains any bracket and a few other weird characters
-    if any(char in text for char in "~()[{}]ãú−-–—‐íÍ►™�şûłìð❤|↑·þ☠î›˛†Şİćğвïâý√ˇœ¯←˘ı¨Б¸˙˜С‹țș˚⅓_"):
+    if any(char in text for char in "~()[{}]ãú−-–—‐íÍ►™�şûłìð❤|↑·þ☠î›˛†Şİćğвïâý√ˇœ¯←˘ı¨Б¸˙˜С‹țș˚⅓_ūēřū"):
         stats["delete_line"] = True
         if verbose: print(f"Line to be deleted - Original: {original_text}")
         return text, stats
