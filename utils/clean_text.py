05e3a680fb3e83803cdb1fe47d377529b052c787
pere <per@capia.no>
2023-09-07 15:12:40 +0200

    Update clean_text.py

--- utils/clean_text.py
+++ utils/clean_text.py
@@ -22,38 +22,74 @@ def clean_text(text, verbose=False):
         return text, stats
 
     original_text = text
-    changes_made = False
 
-    # Initialize a list for logging verbose output
-    verbose_output = []
+    # Double spacing
+    text = ' '.join(text.split())
+    if text != original_text:
+        stats["double_spacing"] += 1
+        if verbose:
+            print(f"Double spacing - Original: {original_text} - Result: {text}")
+            
+    original_text = text
 
-    def apply_change(func, text):
-        nonlocal changes_made
-        cleaned_text = func(text)
-        if cleaned_text != text:
-            stats[func.__name__] += 1
-            changes_made = True
-            if verbose:
-                verbose_output.append(f"{func.__name__} - {text} - {cleaned_text}")
-        return cleaned_text
+    # Remove Dashes
+    text = re.sub(r"^(?:- |— )", "", text)
+    if text != original_text:
+        stats["remove_dashes"] += 1
+        if verbose:
+            print(f"Remove dashes - Original: {original_text} - Result: {text}")
 
-    text = apply_change(lambda t: ' '.join(t.split()), text)
-    text = apply_change(lambda t: re.sub(r"^(?:- |— )", "", t), text)
-    text = apply_change(lambda t: re.sub(r'\.\.\.', '…', t), text)
-    text = apply_change(lambda t: re.sub(r'([!\?])\1+', r'\1', t), text)
-    text = apply_change(lambda t: t.replace('’', "'").replace('ò', 'o').replace('é', 'è').replace('ó', 'ò').replace("1/2", "½").replace("1/4", "¼").replace("3/4", "¾"), text)
-    text = apply_change(lambda t: t.replace("\n", " ").replace("\r", " "), text)
-    text = apply_change(lambda t: t.replace("\t", " "), text)
+    original_text = text
+
+    # Illegal ellipses
+    text = re.sub(r'\.\.\.', '…', text)
+    if text != original_text:
+        stats["illegal_ellipses"] += 1
+        if verbose:
+            print(f"Illegal ellipses - Original: {original_text} - Result: {text}")
+
+    original_text = text
+
+    # Double punctuation
+    text = re.sub(r'([!\?])\1+', r'\1', text)
+    if text != original_text:
+        stats["double_punctuation"] += 1
+        if verbose:
+            print(f"Double punctuation - Original: {original_text} - Result: {text}")
+
+    original_text = text
+
+    # Unicode cleaning
+    text = text.replace('’', "'").replace('ò', 'o').replace('è', 'e').replace('Á', 'A').replace("1/2", "½").replace("1/4", "¼").replace("3/4", "¾").replace('É', 'E').replace('Ò', 'O').replace('æ', 'ae').replace('ø', 'o').replace('å', 'a').replace('Æ', 'AE').replace('Ø', 'O').replace('Å', 'A').replace('°', 'o')
+    if text != original_text:
+        stats["unicode_cleaning"] += 1
+        if verbose:
+            print(f"Unicode cleaning - Original: {original_text} - Result: {text}")
+
+    original_text = text
+
+    # Remove line breaks
+    text = text.replace("\n", " ").replace("\r", " ")
+    if text != original_text:
+        stats["remove_line_breaks"] += 1
+        if verbose:
+            print(f"Remove line breaks - Original: {original_text} - Result: {text}")
+
+    original_text = text
+
+    # Remove tabs
+    text = text.replace("\t", " ")
+    if text != original_text:
+        stats["remove_tabs"] += 1
+        if verbose:
+            print(f"Remove tabs - Original: {original_text} - Result: {text}")
 
     # Stop function
-    allowed_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!"#$%&\'*+,-./:;<=>?@[\\]^_`{|}~—’òèÁ/½¼¾ÉÒæøåÆØÅ°'
+    allowed_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—’òèÁ½¼¾ÉÒæøåÆØÅ°'
     unhandled_char = next((c for c in text if c not in allowed_chars), None)
-    if unhandled_char and not changes_made:
+    if unhandled_char:
         stats["stop_function"] += 1
         print(f"Unhandled character: {unhandled_char}. Original text: {original_text}")
-
-    if verbose:
-        print("\n".join(verbose_output))
 
     return text, stats
 
@@ -64,6 +100,10 @@ if __name__ == "__main__":
     parser.add_argument('--verbose', action='store_true', help='Verbose output of changes.')
     
     args = parser.parse_args()
+    input_file = args.input_file
+    output_file = args.output_file
+
+    df = pd.read_json(input_file, lines=True)
 
     total_stats = {
         "double_spacing": 0,
@@ -76,8 +116,6 @@ if __name__ == "__main__":
         "stop_function": 0
     }
 
-    df = pd.read_json(args.input_file, lines=True)
-
     for index, row in df.iterrows():
         cleaned_text, stats = clean_text(row['text'], args.verbose)
         df.at[index, 'text'] = cleaned_text
@@ -85,7 +123,7 @@ if __name__ == "__main__":
         for key in stats:
             total_stats[key] += stats[key]
 
-    df.to_json(args.output_file, orient='records', lines=True)
+    df.to_json(output_file, orient='records', lines=True)
 
     print("Cleaning completed.")
     print(f"Statistics: {total_stats}")
