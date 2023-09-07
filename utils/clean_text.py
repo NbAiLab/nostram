66fc5410149ddabf2c01c5bf09f93d600cbf6aaa
pere <per@capia.no>
2023-09-07 15:27:33 +0200

    Update clean_text.py

--- utils/clean_text.py
+++ utils/clean_text.py
@@ -2,96 +2,84 @@ import argparse
 import re
 from pandarallel import pandarallel
 import pandas as pd
+import ftfy
 
 # Initialize pandarallel
 pandarallel.initialize()
 
+import ftfy  # Assuming you have imported ftfy
+
 def clean_text(text, verbose=False):
     stats = {
+        "stop_function": 0,
         "double_spacing": 0,
         "remove_dashes": 0,
         "illegal_ellipses": 0,
         "double_punctuation": 0,
         "unicode_cleaning": 0,
         "remove_line_breaks": 0,
-        "remove_tabs": 0,
-        "stop_function": 0
+        "remove_tabs": 0
     }
 
-    if "nocaptions" in text:
-        return text, stats
-
     original_text = text
-
-    # Double spacing
-    text = ' '.join(text.split())
-    if text != original_text:
-        stats["double_spacing"] += 1
-        if verbose:
-            print(f"Double spacing - Original: {original_text} - Result: {text}")
-            
-    original_text = text
-
-    # Remove Dashes
-    text = re.sub(r"^(?:- |— )", "", text)
-    if text != original_text:
-        stats["remove_dashes"] += 1
-        if verbose:
-            print(f"Remove dashes - Original: {original_text} - Result: {text}")
-
-    original_text = text
-
-    # Illegal ellipses
-    text = re.sub(r'\.\.\.', '…', text)
-    if text != original_text:
-        stats["illegal_ellipses"] += 1
-        if verbose:
-            print(f"Illegal ellipses - Original: {original_text} - Result: {text}")
-
-    original_text = text
-
-    # Double punctuation
-    text = re.sub(r'([!\?])\1+', r'\1', text)
-    if text != original_text:
-        stats["double_punctuation"] += 1
-        if verbose:
-            print(f"Double punctuation - Original: {original_text} - Result: {text}")
-
-    original_text = text
-
-    # Unicode cleaning
-    text = text.replace('’', "'").replace('ò', 'o').replace('è', 'e').replace('Á', 'A').replace("1/2", "½").replace("1/4", "¼").replace("3/4", "¾").replace('É', 'E').replace('Ò', 'O').replace('æ', 'ae').replace('ø', 'o').replace('å', 'a').replace('Æ', 'AE').replace('Ø', 'O').replace('Å', 'A').replace('°', 'o')
-    if text != original_text:
-        stats["unicode_cleaning"] += 1
-        if verbose:
-            print(f"Unicode cleaning - Original: {original_text} - Result: {text}")
-
-    original_text = text
-
-    # Remove line breaks
-    text = text.replace("\n", " ").replace("\r", " ")
-    if text != original_text:
-        stats["remove_line_breaks"] += 1
-        if verbose:
-            print(f"Remove line breaks - Original: {original_text} - Result: {text}")
-
-    original_text = text
-
-    # Remove tabs
-    text = text.replace("\t", " ")
-    if text != original_text:
-        stats["remove_tabs"] += 1
-        if verbose:
-            print(f"Remove tabs - Original: {original_text} - Result: {text}")
 
     # Stop function
-    allowed_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—’òèÁ½¼¾ÉÒæøåÆØÅ°'
+    allowed_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—’òèÁ½¼¾ÉÒæøåÆØÅ…°'
     unhandled_char = next((c for c in text if c not in allowed_chars), None)
     if unhandled_char:
         stats["stop_function"] += 1
-        print(f"Unhandled character: {unhandled_char}. Original text: {original_text}")
+        if verbose: print(f"Unhandled character - Original: {original_text} - Char: {unhandled_char}")
+
+    # Unicode cleaning
+    text = ftfy.fix_text(text)
+    if text != original_text:
+        stats["unicode_cleaning"] += 1
+        if verbose: print(f"Unicode cleaning - Original: {original_text} - Result: {text}")
+
+    # Double spacing
+    new_text = ' '.join(text.split())
+    if new_text != text:
+        stats["double_spacing"] += 1
+        if verbose: print(f"Double spacing - Original: {text} - Result: {new_text}")
+        text = new_text
+
+    # Remove Dashes
+    new_text = re.sub(r"^(?:- |— )", "", text)
+    if new_text != text:
+        stats["remove_dashes"] += 1
+        if verbose: print(f"Remove dashes - Original: {text} - Result: {new_text}")
+        text = new_text
+
+    # Illegal ellipses
+    new_text = re.sub(r'\.\.\.', '…', text)
+    if new_text != text:
+        stats["illegal_ellipses"] += 1
+        if verbose: print(f"Illegal ellipses - Original: {text} - Result: {new_text}")
+        text = new_text
+
+    # Double punctuation
+    new_text = re.sub(r'([!\?])\1+', r'\1', text)
+    if new_text != text:
+        stats["double_punctuation"] += 1
+        if verbose: print(f"Double punctuation - Original: {text} - Result: {new_text}")
+        text = new_text
+
+    # Remove line breaks
+    new_text = text.replace("\n", " ").replace("\r", " ")
+    if new_text != text:
+        stats["remove_line_breaks"] += 1
+        if verbose: print(f"Remove line breaks - Original: {text} - Result: {new_text}")
+        text = new_text
+
+    # Remove tabs
+    new_text = text.replace("\t", " ")
+    if new_text != text:
+        stats["remove_tabs"] += 1
+        if verbose: print(f"Remove tabs - Original: {text} - Result: {new_text}")
+        text = new_text
 
     return text, stats
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Clean text in a JSON file.')
