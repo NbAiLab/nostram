85b9f0a3054c8d164dc411eccfdf00ed6f9eed4e
Per E Kummervold <per@capia.no>
2023-09-29 10:10:47 +0200

    merging json script

--- utils/merge_pseudo_labels.py
+++ utils/merge_pseudo_labels.py
@@ -4,86 +4,87 @@ import glob
 import pandas as pd
 from tqdm import tqdm
 
-def simplify_column_names(df, should_simplify=True):
-    if not should_simplify:
-        return df
-    
-    mapping = {
-        'ModelA-NbAiLab-nb-whisper-medium-fine4-npsc-norm-nohes-Norwegian-transcribe-train-transcription': 'A-no-nohes',
-        'ModelB-NbAiLab-nb-whisper-medium-fine3-npsc-norm-raw-Norwegian-transcribe-train-transcription': 'B-no-hes',
-        'ModelC-NbAiLab-nb-whisper-medium-beta-Norwegian-transcribe-train-transcription': 'C-no-medium',
-        'ModelD-NbAiLab-nb-whisper-medium-fine4-npsc-norm-nohes-Nynorsk-transcribe-train-transcription': 'D-nn-nohes',
-        'ModelE-NbAiLab-nb-whisper-medium-fine3-npsc-norm-raw-Nynorsk-transcribe-train-transcription': 'E-nn-hes',
-        'ModelF-NbAiLab-nb-whisper-medium-beta-Nynorsk-transcribe-train-transcription': 'F-nn-medium',
-        'ModelG-NbAiLab-nb-whisper-medium-beta-Norwegian-transcribe-train-transcription': 'G-no-timestamp',
-        'ModelH-openai-whisper-medium-English-translate-train-transcription': 'H-en-timestamp'
-    }
+# Define the mapping of verbose column names to their simplified versions
+mapping = {
+    'ModelA-NbAiLab-nb-whisper-medium-fine4-npsc-norm-nohes-Norwegian-transcribe-train-transcription': 'A-no-nohes',
+    'ModelB-NbAiLab-nb-whisper-medium-fine3-npsc-norm-raw-Norwegian-transcribe-train-transcription': 'B-no-hes',
+    'ModelC-NbAiLab-nb-whisper-medium-beta-Norwegian-transcribe-train-transcription': 'C-no-medium',
+    'ModelD-NbAiLab-nb-whisper-medium-fine4-npsc-norm-nohes-Nynorsk-transcribe-train-transcription': 'D-nn-nohes',
+    'ModelE-NbAiLab-nb-whisper-medium-fine3-npsc-norm-raw-Nynorsk-transcribe-train-transcription': 'E-nn-hes',
+    'ModelF-NbAiLab-nb-whisper-medium-beta-Nynorsk-transcribe-train-transcription': 'F-nn-medium',
+    'ModelG-NbAiLab-nb-whisper-medium-beta-Norwegian-transcribe-train-transcription': 'G-no-timestamp',
+    'ModelH-openai-whisper-medium-English-translate-train-transcription': 'H-en-timestamp'
+}
 
+def simplify_column_names(df):
+    """
+    Simplify the column names of a DataFrame based on a pre-defined mapping.
+    """
     for col in df.columns:
         if col in mapping:
             df.rename(columns={col: mapping[col]}, inplace=True)
-            
     return df
 
 def extract_model_name(filepath):
-    # Extract the part after the second '-' and until the '.'
+    """
+    Extract model name from the filepath.
+    """
     model_name = filepath.split('-', 2)[-1].rsplit('.', 1)[0]
-    # Get parent folder name
     parent_folder = os.path.basename(os.path.dirname(filepath))
-    # Combine parent folder name and model name
     return f"{parent_folder}-{model_name}"
 
 def main(args):
-    print(f"Starting to process {args.input_json}.")
+    """
+    Main function to read JSON and TSV files, merge them, and save to a new JSON file.
+    """
     # Read the main JSON DataFrame
     df_json = pd.read_json(args.input_json, lines=True, dtype={'id': str})
 
-    
-    # Initialize stats
+    # Initialize a list to store added column names
     added_columns = []
-    
+
     # Loop through all TSV files in the input directory and its subdirectories
     for tsv_file in tqdm(glob.glob(os.path.join(args.input_tsv_dir, "**/*.tsv"), recursive=True)):
         
         # Read the current TSV DataFrame
         df_tsv = pd.read_csv(tsv_file, sep='\t')
-        df_tsv['file_id'] = df_tsv['file_id'].astype(str)  # Convert to string
-        second_col = df_tsv.columns[1]
+        df_tsv['file_id'] = df_tsv['file_id'].astype(str)
         
+        # Extract model name from the filepath
         model_name = extract_model_name(tsv_file)
         
-        # Rename the column to match the model_name
-        df_tsv.rename(columns={second_col: model_name}, inplace=True)
+        # Rename the second column to match the extracted model_name
+        df_tsv.rename(columns={df_tsv.columns[1]: model_name}, inplace=True)
         
-        # Check if the column exists, if not create it
+        # Add a new column to the JSON DataFrame if it doesn't exist
         if model_name not in df_json.columns:
-            # print(f"Creating column {model_name}")
             df_json[model_name] = pd.Series(dtype="object")
             added_columns.append(model_name)
-            
-        # Merge DataFrames
+        
+        # Merge JSON and TSV DataFrames
         df_merged = pd.merge(df_json, df_tsv[['file_id', model_name]], left_on='id', right_on='file_id', how='left', suffixes=("", "_new"))
         
-        # Update the original DataFrame
+        # Update values in the main DataFrame
         df_merged[model_name] = df_merged.apply(lambda row: row[model_name + '_new'] if pd.notna(row[model_name + '_new']) else row[model_name], axis=1)
         
-        # Drop the new column and 'file_id'
+        # Drop redundant columns
         df_merged.drop(columns=[model_name + '_new', 'file_id'], inplace=True)
         
         # Update the main DataFrame
         df_json = df_merged
-        
-
-    # Simplify column names
-    df_json = simplify_column_names(df_json, not args.do_not_simplify_columns)
-
-    # Write the final JSON file
-    df_json.to_json(args.output_json, orient='records', lines=True)
     
-    # Print statistics
+    # Simplify column names if the flag is not set
+    if not args.do_not_simplify_columns:
+        simplify_column_names(df_json)
+    
+    # Save the merged DataFrame to a new JSON file
+    df_json.to_json(args.output_json, orient='records', lines=True)
+
+    # Print some statistics
     print(f"Total rows in the output file: {len(df_json)}")
     for col in added_columns:
-        print(f"{col}: {df_json[col].count()} filled values")
+        simplified_col = col if args.do_not_simplify_columns else mapping.get(col, col)
+        print(f"{simplified_col}: {df_json[simplified_col].count()} non-NaN values")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge JSON and TSV files")
@@ -92,6 +93,5 @@ if __name__ == "__main__":
     parser.add_argument("--output_json", type=str, required=True, help="Path to the output JSON file")
     parser.add_argument("--do_not_simplify_columns", action='store_true', help="Flag to prevent column name simplification")
 
-    
     args = parser.parse_args()
     main(args)
---
+++ utils/post_clean.py
@@ -0,0 +1,250 @@
+# Introduction
+# This script performs text analysis on a given TSV file. It identifies various statistics
+# related to text prediction such as the minimum and maximum number of words predicted,
+# whether the first and last words are correctly predicted, and more. The script is parallelized
+# for performance.
+
+import argparse, string,re, os, sys
+from collections import Counter
+from pandarallel import pandarallel
+from tqdm import tqdm
+import pandas as pd
+import logging
+import json
+import time
+from jiwer import wer
+from fuzzywuzzy import fuzz
+
+start_time = time.time()
+
+
+def exec_time():
+    end_time = time.time()
+    out = str(round(end_time - start_time, 1)) + " seconds"
+    return out
+
+def load_json(jsonline):
+    data = pd.read_json(jsonline, lines=True)
+
+    logger.info(f'***  Json parsed. {len(data)} lines. ({exec_time()})')
+    print(f'***  Json parsed with {len(data)} lines. ({exec_time()})')
+
+    return data
+
+
+def read_config(cfile):
+    try:
+        f = open(cfile, "r")
+        config = json.load(f)
+    except:
+        logger.info(
+            "Error. There has to be a valid config-file in the output directory")
+        print("Error. There has to be a valid config-file in the output directory")
+        exit()
+
+    return config
+
+
+def save_json(data, filename):
+    with open(filename, 'w', encoding='utf-8') as file:
+        data.to_json(file, orient='records', lines=True, force_ascii=False)
+    logger.info(f'Saved jsonl as "{filename}"')
+
+# Function to find insertions that do not exist in the predictions.
+def find_insertions(target, predictions, min_length=1, max_length=50):
+    target_words = set(str(target).lower().split())
+    ngram_not_in_pred = ""
+    max_length = 0
+    # Loop through each prediction to find insertions.
+    for prediction in predictions:
+        prediction_words = str(prediction).lower().split()
+        ngram_counter = Counter()
+        # Generate n-grams
+        for n in range(min_length, max_length + 1):
+            ngrams = [' '.join(prediction_words[i:i+n]) for i in range(len(prediction_words) - n + 1)]
+            ngram_counter.update(ngrams)
+        # Check if n-grams exist in target
+        for ngram, count in ngram_counter.items():
+            ngram_words = set(ngram.split())
+            if not ngram_words.issubset(target_words):
+                length = len(ngram_words)
+                if length > max_length:
+                    ngram_not_in_target = ngram
+                    max_length = length
+    return ngram_not_in_pred, max_length
+
+# Function to find common n-grams not in target
+def find_common_sequence(predictions, target, min_length=3, max_length=50):
+    # Prepare target words
+    target_words = str(target).lower().translate(str.maketrans('', '', string.punctuation)).split()
+    def get_ngrams(words, n):
+        return [' '.join(words[i:i+n]) for i in range(len(words) - n + 1)]
+    predictions_ngrams = []
+    # Loop through each prediction to find common sequences.
+    for prediction in predictions:
+        prediction_words = str(prediction).lower().translate(str.maketrans('', '', string.punctuation)).split()
+        ngrams = []
+        for n in range(min_length, max_length + 1):
+            ngrams.extend(get_ngrams(prediction_words, n))
+        predictions_ngrams.append(set(ngrams))
+    # Intersect to find common n-grams
+    common_ngrams = set.intersection(*predictions_ngrams[:2])
+    for prediction_ngrams in predictions_ngrams[2:]:
+        common_ngrams = common_ngrams.intersection(prediction_ngrams)
+        if len(common_ngrams) == 0:
+            break
+    ngram_not_in_target = ""
+    best_length = 0
+    # Identify the best n-gram based on length
+    for ngram in common_ngrams:
+        if all(word not in target_words for word in ngram.split()) and len(ngram.split()) > best_length:
+            ngram_not_in_target = ngram
+            best_length = len(ngram.split())
+    return ngram_not_in_target, best_length
+
+# Text cleaning function
+def clean_text(text):
+    return re.sub(r'[^a-zA-ZæøåÆØÅ]', ' ', str(text).lower()).strip()
+
+# Function to analyze a single row of data
+def analyze_row(row, *config):
+    
+    min_length = config[0]['min_seq_length']
+    max_length = config[0]['max_seq_length']
+
+    # Process target and prediction data
+   
+    clean_target = clean_text(row['text'])
+    lang_cols = config[0]['predictions'][row["text_language"]]
+    
+    predictions = row[lang_cols]
+    clean_predictions = [clean_text(p) for p in predictions]
+    
+    # Find insertions and sequences
+    ngram_not_in_pred, max_ngram_not_in_pred = find_insertions(clean_target, predictions, min_length, max_length)
+    ngram_not_in_target, max_ngram_not_in_target = find_common_sequence(predictions, clean_target, min_length, max_length)
+    
+    # Check if first and last words are predicted
+    first_word = clean_target.split()[0] if clean_target else ""
+    last_word = clean_target.split()[-1] if clean_target else ""
+    
+    #Fuzzy match
+    fuzz_threshold = 80
+    first_word_predicted = int(any(fuzz.ratio(first_word, p.split()[0]) >= fuzz_threshold if p.split() else False for p in clean_predictions))
+    last_word_predicted = int(any(fuzz.ratio(last_word, p.split()[-1]) >= fuzz_threshold if p.split() else False for p in clean_predictions))
+
+    
+    #Exact match
+    first_word_predicted_exact = int(any(first_word == p.split()[0] if p.split() else False for p in clean_predictions))
+    last_word_predicted_exact = int(any(last_word == p.split()[-1] if p.split() else False for p in clean_predictions))
+ 
+    if first_word_predicted != first_word_predicted_exact:
+        print(f"First word: {first_word} - Clean: {clean_predictions}")
+
+    if last_word_predicted != last_word_predicted_exact:
+        print(f"Last word: {last_word} - Clean: {clean_predictions}")
+    
+    # Find min and max words in predictions
+    min_words = min(len(p.split()) for p in clean_predictions) if clean_predictions else 0
+    max_words = max(len(p.split()) for p in clean_predictions) if clean_predictions else 0
+    
+    whisper_models = lang_cols
+    # Calculate the WER-score for the tested models (clean_target vs clean_predictions)
+    whisper_wer_scores = [wer(clean_target, p) for p in clean_predictions]
+    
+    # Find the best of the WER scores and the corresponding model
+    if whisper_wer_scores:
+        whisper_wer = min(whisper_wer_scores)
+        best_model_index = whisper_wer_scores.index(whisper_wer)
+        whisper_best_model = whisper_models[best_model_index]
+    else:
+        whisper_wer = 1
+        whisper_best_model = None
+    
+    
+    # Return results
+    return pd.Series([max_words, min_words, last_word_predicted, first_word_predicted, ngram_not_in_target, max_ngram_not_in_target, ngram_not_in_pred, max_ngram_not_in_pred, whisper_wer, whisper_models, whisper_wer_scores, whisper_best_model])
+
+
+# Main function to execute the script
+def main(args):
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.max_colwidth", None)
+
+    # Invoke logging
+    log_name = os.path.basename(args.input_filename).replace(".json", "") + ".log"
+
+    # Create directories if they do not exist
+    if not os.path.exists(args.output_folder + "/log"):
+        print(args.output_folder)
+        os.makedirs(args.output_folder + "/log")
+
+    handler = logging.FileHandler(
+        filename=os.path.join(args.output_folder, "log/", log_name),
+        mode='w'
+    )
+    formatter = logging.Formatter('%(asctime)s %(message)s')
+    handler.setFormatter(formatter)
+    handler.setLevel(
+        {"DEBUG": logging.DEBUG, "INFO": logging.INFO}[args.log_level])
+    logger.addHandler(handler)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+
+    config_file_path = os.path.join(
+        args.output_folder.replace("/train", "/").replace("/test", "/").replace("/validation", "/"), "config.json")
+    config = read_config(config_file_path)
+
+    print(f'*** Starting to process: {args.input_filename}')
+    
+    # Read data and prepare columns
+    data: pd.DataFrame = load_json(args.input_filename)
+    data = data.fillna('')
+    
+    new_cols = ['max_words_predicted', 'min_words_predicted', 'last_word_predicted', 'first_word_predicted', 'ngram_not_in_target','max_ngrams_not_in_target', 'ngram_not_in_pred','max_ngrams_not_in_pred', 'whisper_wer', 'whisper_models', 'whisper_wer_scores', 'whisper_best_model']
+    for col in reversed(new_cols):
+        if col not in data.columns:
+            data.insert(data.columns.get_loc('text'), col, 0)
+        else:
+            data[col] = 0
+    
+    # Initialize pandarallel with progress_bar
+    pandarallel.initialize(progress_bar=True)
+    
+    # Perform parallel analysis
+    data[new_cols] = data.apply(analyze_row, axis=1, args=(config,))
+
+    #data[new_cols] = data.parallel_apply(analyze_row, axis=1, args=(config,))
+    
+    # Save it as jsonl
+    output_filename = os.path.join(
+        args.output_folder, os.path.basename(args.input_filename))
+    save_json(data, output_filename)
+    logger.info(
+        f'*** Finished processing file.'
+        f'\nResult is written to {os.path.join(args.output_folder, os.path.basename(args.input_filename))}. ({exec_time()})')
+    
+# Entry point of the script
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_filename", required=True, help="Input JSON lines file")
+    parser.add_argument("--output_folder", required=True, help="Output folder")
+    parser.add_argument('--log_level', required=False, default="INFO",
+                        help='Set logging level to DEBUG to get a report on all decisions')
+    args = parser.parse_args()
+    
+        # Invoke logger globally
+    logger = logging.getLogger()
+
+    if args.log_level == "INFO":
+        logger.setLevel(logging.INFO)
+    elif args.log_level == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+    else:
+        print("Log level not accepted")
+        exit()
+    
+    main(args)
