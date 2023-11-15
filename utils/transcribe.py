a409e7250a0a864a464fd61ba2100dfd5e37703b
Per E Kummervold <per@capia.no>
2023-11-15 20:08:00 +0100

    update script for transcribe

--- utils/transcribe.py
+++ utils/transcribe.py
@@ -3,7 +3,7 @@ import tempfile
 from transformers import pipeline
 from transformers import WhisperProcessor, WhisperForConditionalGeneration, WhisperConfig
 
-def main(model_path, audio_path, commit_hash=None,task="transcribe",language="no",num_beams=1):
+def main(model_path, audio_path, commit_hash=None,task="transcribe",language="no",num_beams=1,chunk_length=30):
     with tempfile.TemporaryDirectory() as tmp_dir:
         # Load the model and processor
         if commit_hash:
@@ -24,8 +24,9 @@ def main(model_path, audio_path, commit_hash=None,task="transcribe",language="no
         asr = pipeline("automatic-speech-recognition", model=tmp_dir, tokenizer=tmp_dir)
 
         # Process the audio and print results
-        result = asr(audio_path, return_timestamps=True, chunk_length_s=30, generate_kwargs={'task': task, 'language': language, 'num_beams': num_beams})
+        result = asr(audio_path, return_timestamps=True, chunk_length_s=chunk_length, generate_kwargs={'task': task, 'language': language, 'num_beams': num_beams})
         
+        breakpoint()
         text = result['text']
         word_count = len(text.split())
 
@@ -37,9 +38,10 @@ if __name__ == "__main__":
     parser.add_argument("--model_path", type=str, required=True, help="Path to the Whisper model")
     parser.add_argument("--audio_path", type=str, required=True, help="Path to the audio file")
     parser.add_argument("--num_beams", type=int, default=1, help="Number of beams (default: 1)")
+    parser.add_argument("--chunk_length", type=int, default=30, help="Chunk length (default: 30)")
     parser.add_argument("--task", type=str, default="transcribe", choices=["transcribe", "translate"], help="Task to perform: 'transcribe' or 'translate' (default: transcribe)")
     parser.add_argument("--language", type=str, default="no", choices=["no", "nn", "en"], help="Target language: 'no', 'nn' or 'translate' (default: no)")
     parser.add_argument("--commit_hash", type=str, default=None, help="Specific commit hash for the model (optional)")
     args = parser.parse_args()
 
-    main(args.model_path, args.audio_path, args.commit_hash,args.task,args.language,args.num_beams)
+    main(args.model_path, args.audio_path, args.commit_hash,args.task,args.language,args.num_beams,args.chunk_length)
