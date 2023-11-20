a6038d1602bab8d3da79a3ceb420836116ea93c7
Per E Kummervold <per@capia.no>
2023-11-20 11:58:53 +0100

    dual

--- webapp/nbwhisperdemo_video_debug.py
+++ webapp/nbwhisperdemo_video_debug.py
@@ -376,6 +376,8 @@ if __name__ == "__main__":
         return file_contents, file_path
     
     def create_transcript_file(text, file_path, return_timestamps, transcription_style="semantic"):
+        print("hei")
+        print(transcription_style)
         if return_timestamps:
             transcript_content = format_to_vtt(text, return_timestamps, transcription_style=transcription_style)
             subtitle_display = re.sub(r"\.[^.]+$", "_middle.vtt", file_path)
