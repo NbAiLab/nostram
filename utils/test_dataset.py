1949d7fb043a74bc0e1a3a84aa67a68db293235b
Per E Kummervold <per@capia.no>
2024-01-25 17:39:42 +0100

    test dataset

--- utils/test_dataset.py
+++ utils/test_dataset.py
@@ -6,7 +6,8 @@ from datasets import load_dataset
 import os
 import warnings
 import jiwer
-import logging
+import json
+from datetime import datetime
 
 # Suppress specific warning categories
 warnings.filterwarnings('ignore', category=UserWarning)
@@ -38,7 +39,7 @@ def calculate_wer(references, predictions):
     normalized_predictions = [normalizer(pred) for pred in predictions]
     return jiwer.wer(normalized_references, normalized_predictions)
 
-def process_audio_data(dataset_path, split, model_path, num_examples, task, language, print_predictions, calculate_wer_flag):
+def process_audio_data(dataset_path, split, model_path, num_examples, task, language, print_predictions, calculate_wer_flag, save_file):
     dataset = load_dataset(dataset_path, split=split, streaming=True)
     processor = WhisperProcessor.from_pretrained(model_path, from_flax=False)
     model = WhisperForConditionalGeneration.from_pretrained(model_path, from_flax=False)
@@ -48,15 +49,11 @@ def process_audio_data(dataset_path, split, model_path, num_examples, task, lang
 
     references = []
     predictions = []
-    example_count = 0
-    
+
     for idx, example in enumerate(dataset):
-        
         if idx >= num_examples:
             break
-        
-        example_count += 1
-        
+
         waveform = np.array(example["audio"]["array"], dtype=np.float32)
         sampling_rate = example["audio"]["sampling_rate"]
         input_features = processor(waveform, sampling_rate=sampling_rate, return_tensors="pt").input_features.to(device)
@@ -73,18 +70,31 @@ def process_audio_data(dataset_path, split, model_path, num_examples, task, lang
 
     if calculate_wer_flag:
         overall_wer = calculate_wer(references, predictions)
-        print(f"Average WER for {example_count} examples: {overall_wer:.2f}")
+        print(f"Average WER: {overall_wer:.2f}")
+
+        if save_file:
+            result = {
+                "model_path": model_path,
+                "dataset_path": dataset_path,
+                "split": split,
+                "date": datetime.now().strftime("%Y-%m-%d %H:%M"),
+                "example_count": num_examples,
+                "wer": overall_wer
+            }
+            with open(save_file, 'a') as f:
+                f.write(json.dumps(result) + "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process audio data using a Whisper model.")
     parser.add_argument("--dataset_path", type=str, required=True, help="Path or identifier to the dataset.")
     parser.add_argument("--split", type=str, required=True, help="Dataset split to use (train, test, validation).")
     parser.add_argument("--model_path", type=str, required=True, help="Path to the pre-trained Whisper model.")
-    parser.add_argument("--num_examples", type=int, default=999999999, help="Number of examples to process.")
+    parser.add_argument("--num_examples", type=int, default=10, help="Number of examples to process.")
     parser.add_argument("--task", type=str, default="transcribe", help="Transcribe, translate or both.")
     parser.add_argument("--language", type=str, default="no", help="Specify language (ie no, nn or en) if you want to override the setting in the dataset.")
     parser.add_argument("--print_predictions", action="store_true", help="Print predictions if set.")
     parser.add_argument("--calculate_wer", action="store_true", help="Calculate WER if set.")
+    parser.add_argument("--save_file", type=str, help="Path to save results in JSON Lines format.")
     
     args = parser.parse_args()
-    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language, args.print_predictions, args.calculate_wer)
+    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language, args.print_predictions, args.calculate_wer, args.save_file)
