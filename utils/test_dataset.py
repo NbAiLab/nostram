942d36fd18944919f6c7c536da29b354ec4c2d06
Per E Kummervold <per@capia.no>
2024-01-25 17:21:32 +0100

    test dataset

--- utils/test_dataset.py
+++ utils/test_dataset.py
@@ -5,14 +5,14 @@ import torch
 from datasets import load_dataset
 import os
 import warnings
-import logging
+import jiwer
 
 # Suppress specific warning categories
 warnings.filterwarnings('ignore', category=UserWarning)
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 # Set TensorFlow logging to error level only
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow logging (1 = INFO, 2 = WARNING, 3 = ERROR)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
 # Set other logging levels
@@ -22,11 +22,22 @@ logging.getLogger('datasets').setLevel(logging.ERROR)
 # Just needed if the dataset requires authentication
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/perk/service_account_nancy.json"
 
-def calculate_word_error_rate(reference, hypothesis):
-    """Calculate Word Error Rate between reference and hypothesis."""
-    return wer(reference, hypothesis)
+def normalizer(text):
+    transformation = jiwer.Compose([
+        jiwer.ToLowerCase(),
+        jiwer.RemoveMultipleSpaces(),
+        jiwer.Strip(),
+        jiwer.RemovePunctuation(),
+        jiwer.RemoveWhiteSpace(replace_by_space=True)
+    ])
+    return transformation(text)
 
-def process_audio_data(dataset_path, split, model_path, num_examples, task, language, print_predictions, calculate_wer):
+def calculate_wer(references, predictions):
+    normalized_references = [normalizer(ref) for ref in references]
+    normalized_predictions = [normalizer(pred) for pred in predictions]
+    return jiwer.wer(normalized_references, normalized_predictions)
+
+def process_audio_data(dataset_path, split, model_path, num_examples, task, language, print_predictions, calculate_wer_flag):
     dataset = load_dataset(dataset_path, split=split, streaming=True)
     processor = WhisperProcessor.from_pretrained(model_path, from_flax=False)
     model = WhisperForConditionalGeneration.from_pretrained(model_path, from_flax=False)
@@ -34,7 +45,8 @@ def process_audio_data(dataset_path, split, model_path, num_examples, task, lang
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    total_wer = 0
+    references = []
+    predictions = []
 
     for idx, example in enumerate(dataset):
         if idx >= num_examples:
@@ -44,21 +56,19 @@ def process_audio_data(dataset_path, split, model_path, num_examples, task, lang
         sampling_rate = example["audio"]["sampling_rate"]
         input_features = processor(waveform, sampling_rate=sampling_rate, return_tensors="pt").input_features.to(device)
 
-
-        # Generate the token IDs using the model for either transcription or translation
         predicted_ids = model.generate(input_features, task=task, language=language, return_timestamps=True, max_new_tokens=256)
         transcription = processor.batch_decode(predicted_ids, decode_with_timestamps=False, skip_special_tokens=True)[0]
 
         if print_predictions:
             print(f"| {example['text']} | {transcription} |")
 
-        if calculate_wer:
-            total_wer += calculate_word_error_rate(example['text'], transcription)
+        if calculate_wer_flag:
+            references.append(example['text'])
+            predictions.append(transcription)
 
-    if calculate_wer:
-        average_wer = total_wer / num_examples
-        print(f"Average WER: {average_wer:.2f}")
-
+    if calculate_wer_flag:
+        overall_wer = calculate_wer(references, predictions)
+        print(f"Average WER: {overall_wer:.2f}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process audio data using a Whisper model.")
@@ -70,9 +80,6 @@ if __name__ == "__main__":
     parser.add_argument("--language", type=str, default="no", help="Specify language (ie no, nn or en) if you want to override the setting in the dataset.")
     parser.add_argument("--print_predictions", action="store_true", help="Print predictions if set.")
     parser.add_argument("--calculate_wer", action="store_true", help="Calculate WER if set.")
-
     
     args = parser.parse_args()
-    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language,args.print_predictions, args.calculate_wer)
-
-
+    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language, args.print_predictions, args.calculate_wer)
