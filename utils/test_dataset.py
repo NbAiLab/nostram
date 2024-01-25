07d27171125ffd50fc980169209bcb43055679dd
Per E Kummervold <per@capia.no>
2024-01-25 16:14:26 +0100

    test dataset

--- utils/test_dataset.py
+++ utils/test_dataset.py
@@ -56,12 +56,20 @@ def process_audio_data(dataset_path, split, model_path, num_examples, task, lang
         average_wer = total_wer / num_examples
         print(f"Average WER: {average_wer:.2f}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process audio data using a Whisper model.")
-    
-    # [Existing argument parser code...]
+    parser.add_argument("--dataset_path", type=str, required=True, help="Path or identifier to the dataset.")
+    parser.add_argument("--split", type=str, required=True, help="Dataset split to use (train, test, validation).")
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the pre-trained Whisper model.")
+    parser.add_argument("--num_examples", type=int, default=10, help="Number of examples to process.")
+    parser.add_argument("--task", type=str, default="transcribe", help="Transcribe, translate or both.")
+    parser.add_argument("--language", type=str, default="auto", help="Specify language (ie no, nn or en) if you want to override the setting in the dataset.")
     parser.add_argument("--print_predictions", action="store_true", help="Print predictions if set.")
     parser.add_argument("--calculate_wer", action="store_true", help="Calculate WER if set.")
 
+    
     args = parser.parse_args()
-    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language, args.print_predictions, args.calculate_wer)
+    process_audio_data(args.dataset_path, args.split, args.model_path, args.num_examples, args.task, args.language)
+
+
