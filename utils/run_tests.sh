d1fb6f2ed00b6fdf8d0de11025c35bc55c3e8cb1
Per E Kummervold <per@capia.no>
2024-01-26 10:46:25 +0100

    test

--- utils/run_tests.sh
+++ utils/run_tests.sh
@@ -2,9 +2,7 @@
 
 # Define the array of model paths
 #model_paths=("openai/whisper-tiny" "openai/whisper-base" "openai/whisper-small" "openai/whisper-medium" "openai/whisper-large-v3" "NbAiLabBeta/nb-whisper-tiny" "NbAiLabBeta/nb-whisper-base" "NbAiLabBeta/nb-whisper-small" "NbAiLabBeta/nb-whisper-medium" "NbAiLabBeta/nb-whisper-large")
-
-model_paths=("NbAiLabBeta/nb-whisper-tiny" "NbAiLabBeta/nb-whisper-base")
-
+model_paths=("openai/whisper-large","openai/whisper-large-v2" "openai/whisper-large-v3")
 
 # Define the array of splits
 splits=("test_norwegian_fleurs" "test_nst")
@@ -17,3 +15,5 @@ for model_path in "${model_paths[@]}"; do
         python test_dataset.py --dataset_path NbAiLab/ncc_speech_v7 --split "$split" --model_path "$model_path" --calculate_wer --save_file /home/perk/results.jsonl
     done
 done
+
+
